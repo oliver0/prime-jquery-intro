9ee032b39a46db41f16134dffe404f2b72b50ebd
Kris Szafranski <kdszafranski@gmail.com>
2016-07-05 14:22:22 -0500

    added in consoles to demo loading on document ready

--- app.js
+++ app.js
@@ -1,28 +1,32 @@
 $(document).ready(function() {
-    // Selectors
-    // Class and ID
-    // $('#example').css('background-color', 'yellow');
-    // $('.inner-list').css('background-color', 'red');
+  console.log('First?');
 
-    // direct descendants
-    // $('div > h2').css('background-color', 'purple');
+  // Selectors
+  // Class and ID
+  // $('#example').css('background-color', 'yellow');
+  // $('.inner-list').css('background-color', 'red');
 
-    // Multiples
-    // $('.my-list li p, #example li p').css('background-color', 'pink');
+  // direct descendants
+  // $('div > h2').css('background-color', 'purple');
 
-    // Find
-    // $('li').find('p').css('background-color', 'orange');
+  // Multiples
+  // $('.my-list li p, #example li p').css('background-color', 'pink');
 
-      // Traversal/first and last
-    // $('ul').last().css('background-color', 'pink');
-    // $('p').parent().css('background-color', 'blue');
+  // Find
+  // $('li').find('p').css('background-color', 'orange');
 
-    // chaining
-    // $('.my-list p').last().parent().css('background-color', 'peachpuff');
+    // Traversal/first and last
+  // $('ul').last().css('background-color', 'pink');
+  // $('p').parent().css('background-color', 'blue');
 
-    // Psuedo Selectors
-    $('p:even').css('background-color', 'green');
+  // chaining
+  // $('.my-list p').last().parent().css('background-color', 'peachpuff');
+
+  // Psuedo Selectors
+  $('p:even').css('background-color', 'green');
 
 
 
 });
+
+console.log('When does this happen?');
