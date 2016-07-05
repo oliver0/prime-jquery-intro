5b4c19ccb3ac2a037e2f6d54c3110ea94c8a1816
Kris Szafranski <kdszafranski@gmail.com>
2016-07-05 15:13:44 -0500

    added filter, display:none to selectors code

--- selectors/app.js
+++ selectors/app.js
@@ -3,29 +3,34 @@ $(document).ready(function() {
 
   // Selectors
   // Class and ID
-  $('#example').css('background-color', 'yellow');
+  // $('#example').css('background-color', 'yellow');
   // $('.inner-list').css('background-color', 'red');
 
-  // direct descendants
+  // Direct Descendants
   // $('div > h2').css('background-color', 'purple');
 
-  // Multiples
+  // Multiple selections
   // $('.my-list li p, #example li p').css('background-color', 'pink');
 
   // Find
   // $('li').find('p').css('background-color', 'orange');
+  // $('li').find('p').css('display', 'none');
 
-    // Traversal/first and last
-  // $('ul').last().css('background-color', 'pink');
-  // $('p').parent().css('background-color', 'blue');
+  // Traversal: First and Last
+  // $('ul').first().css('background-color', 'pink');
+  // $('ul').last().css('background-color', 'peachpuff');
 
-  // Relative, .next(), .closest()
+  // Traversal: Dynamic and Relative positions, .parent(), .sibling(), .children(), .next(), .closest()
+  // Method Chaining
 
-  // chaining
+  // $('.inner-p').parent().parent().css('background-color', 'blue');
   // $('.my-list p').last().parent().css('background-color', 'peachpuff');
 
   // Psuedo Selectors
   // $('p:even').css('background-color', 'green');
+
+  // Filtering
+  // $('li').filter(':even').css( 'background-color', 'red' );
 
 
 
